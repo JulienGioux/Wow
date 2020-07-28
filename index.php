5f55de25c8af46e7c887f3a6b2f297e8a545985f
unknown <jules.gioux@gmail.com>
2020-07-28 14:25:04 +0200

    test

--- index.php
+++ index.php
@@ -11,46 +11,70 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>WoW Alpha_0.01</title>
 </head>
-<body>
+<body class="container-fluid">
+    <section class="row">
+        <div class="card mb-3 mx-3 col-md-5">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <p>
+                            <?php $myHero = new Hero(2000, 0, 'excalibure', 250, 'Le bouclier d\'Achille', 600);?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3 mx-3 col-md-5">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <p>
+                    <?php $myOrc = new Orc(800, 0);?>  
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <div>
-        <p>
-            <?php $myHero = new Hero(2000, 0, 'excalibure', 250, 'Le bouclier d\'Achille', 600);?>
-        </p>
-        <p>
-            <?php $myOrc = new Orc(800, 0);?>  
-        </p>
         <?php
             while ($myHero->getHealth() > 0 && $myOrc->getHealth() > 0) {
         ?>
         <p>
         <?php
-                $myOrc->attack();
-                $myHero->attacked($myOrc->getDamage());
-                $myHero->moreRage();
-                echo 'Un Orc vous a infligé ' . $myOrc->getDamage() . ' points d\'attaque <br>';
-                if ($myOrc->getDamage() > $myHero->getShieldValue()) {
-                    $ptsAbs = $myHero->getShieldValue();
-                    $ptsHealth = abs($myHero->getShieldValue() - $myOrc->getDamage());
-                } else {
-                    $ptsAbs = $myHero->getShieldValue() - $myOrc->getDamage();
-                    $ptsHealth = 0;
-                }
-                echo 'Votre armure a absorbé ' . $ptsAbs . ' points d\'attaque.<br>';
-                echo 'vous avez perdu ' . $ptsHealth . ' points de vie.<br>';
-                $heroHealth = ($myHero->getHealth() > 0) ? $myHero->getHealth() : 0;
-                echo 'Il reste ' . $heroHealth . ' points de vie à votre Héro. <br>';
-                echo 'Votre Héro a maintenant ' . $myHero->getRage() . ' points de rage ! <br>';
+            $myOrc->attack();
+            $myHero->attacked($myOrc->getDamage());
+            $myHero->moreRage();
+            echo 'Un Orc vous a infligé ' . $myOrc->getDamage() . ' points d\'attaque <br>';
+            if ($myOrc->getDamage() > $myHero->getShieldValue()) {
+                $ptsAbs = $myHero->getShieldValue();
+                $ptsHealth = abs($myHero->getShieldValue() - $myOrc->getDamage());
+            } else {
+                $ptsAbs = $myHero->getShieldValue() - $myOrc->getDamage();
+                $ptsHealth = 0;
+            }
+            echo 'Votre armure a absorbé ' . $ptsAbs . ' points d\'attaque.<br>';
+            echo 'vous avez perdu ' . $ptsHealth . ' points de vie.<br>';
+            $heroHealth = ($myHero->getHealth() > 0) ? $myHero->getHealth() : 0;
+            echo 'Il reste ' . $heroHealth . ' points de vie à votre Héro. <br>';
+            echo 'Votre Héro a maintenant ' . $myHero->getRage() . ' points de rage ! <br>';
         ?>
         </p>
         <p>
         <?php
-                if ($myHero->getRage() >= 100 && $myHero->getHealth() > 0) {
-                    $myOrc->setHealth($myOrc->getHealth() - $myHero->getWeaponDamage());
-                    $myHero->setRage(0);
-                    echo 'Votre Héro est enragé et inflige ' . $myHero->getWeaponDamage() . ' points de vie à l\'abominable Orc des neiges ! <br>';
-                    $orcHealth = ($myOrc->getHealth() > 0) ? $myOrc->getHealth() : 0;
-                    echo 'L\'abominable Orc des neiges a maintenant ' . $orcHealth . ' points de vie.<br>';
-                }
+            if ($myHero->getRage() >= 100 && $myHero->getHealth() > 0) {
+                $myOrc->setHealth($myOrc->getHealth() - $myHero->getWeaponDamage());
+                $myHero->setRage(0);
+                echo 'Votre Héro est enragé et inflige ' . $myHero->getWeaponDamage() . ' points de vie à l\'abominable Orc des neiges ! <br>';
+                $orcHealth = ($myOrc->getHealth() > 0) ? $myOrc->getHealth() : 0;
+                echo 'L\'abominable Orc des neiges a maintenant ' . $orcHealth . ' points de vie.<br>';
+            }
         ?>
         <p>
         <?php
